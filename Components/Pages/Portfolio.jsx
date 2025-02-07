556d74728f9e7ec934f935b8005e716c5a0e50c6
Massive12356 <edwardmintah17@gmail.com>
2025-02-07 08:45:03 +0000

    updated portfolio section

--- Components/Pages/Portfolio.jsx
+++ Components/Pages/Portfolio.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Mentalhealth from '../../src/assets/mentalHealth.png'
-import newsLetter from '../../src/assets/newsLetter.png'
-import AgeCalProject from '../../src/assets/AgeCal.png'
 import estatePic from '../../src/assets/estate.png'
 import  pic1 from '../../src/assets/construction pic.png';
 import  Logo from '../../src/assets/external-link-svgrepo-com.svg';
@@ -33,22 +31,6 @@ const Portfolio = () => {
                     <h4>CONSTRUCTION WEBSITE</h4>
                     <p>A user-friendly construction website highlighting services, past projects, and client testimonials to build trust and engagement.</p>
                     <a href="https://construction-app-nine.vercel.app/" target="_blank"><img src={Logo} alt="External Link" className="link" /></a>
-                </div>
-            </div>
-            <div className="portfolio-box">
-                <img src={newsLetter} alt=""/>
-                <div className="portfolio-layer">
-                    <h4>NEWS LETTER SIGNUP PAGE</h4>
-                    <p>This  newsletter sign-up page is a web page designed to collect user email addresses for subscribing to regular updates, promotions, or content</p>
-                    <a href="https://massive12356.github.io/Frontend-Challenge/newsletter-sign-up-with-success-message-main/" target="_blank"><img src={Logo} alt="External Link" className="link" /></a>
-                </div>
-            </div>
-            <div className="portfolio-box">
-                <img src={AgeCalProject} alt=""/>
-                <div className="portfolio-layer">
-                    <h4>AGE CALCULATOR</h4>
-                    <p>This age calculator determines a person's age based on their birthdate by calculating the difference between the current date and the provided birthdate.</p>
-                    <a href="https://massive12356.github.io/Frontend-Challenge/age-calculator-app-main/" target="_blank"><img src={Logo} alt="External Link" className="link" /></a>
                 </div>
             </div>
         </div>
